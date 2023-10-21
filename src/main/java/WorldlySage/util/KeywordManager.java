07323f9c1ed5e07531bf78391c69e3f5ab395af3
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-21 08:19:17 -0300

    Merge old ability cards from Hostess. Trim keywords

--- src/main/java/WorldlySage/util/KeywordManager.java
+++ src/main/java/WorldlySage/util/KeywordManager.java
@@ -8,8 +8,8 @@ public class KeywordManager {
     public static String BRACED;
     public static String EXPOSED;
     public static String STAGGER;
-    public static String GATHER;
     public static String INFUSE;
+    public static String GATHER;
     public static String ABILITY;
 
     public static String getKeyword(String ID) {
