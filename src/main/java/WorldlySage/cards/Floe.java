2c77d54fdbab7623551b75949d2ad3e182545b2e
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-01-15 23:19:32 -0400

    Add IceWave, Floe

---
+++ src/main/java/WorldlySage/cards/Floe.java
@@ -0,0 +1,39 @@
+package WorldlySage.cards;
+
+import WorldlySage.cards.abstracts.AbstractEasyCard;
+import WorldlySage.util.Wiz;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.green.Defend_Green;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static WorldlySage.MainModfile.makeID;
+
+public class Floe extends AbstractEasyCard {
+    public final static String ID = makeID(Floe.class.getSimpleName());
+
+    public Floe() {
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
+        baseBlock = block = 7;
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        Wiz.atb(new DrawCardAction(magicNumber));
+        Wiz.atb(new DiscardAction(p, p, magicNumber, false));
+    }
+
+    @Override
+    public void upp() {
+        upgradeBlock(1);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Defend_Green.ID;
+    }
+}
---
+++ src/main/java/WorldlySage/cards/IceWave.java
@@ -0,0 +1,37 @@
+package WorldlySage.cards;
+
+import WorldlySage.cards.abstracts.AbstractAbilityCard;
+import WorldlySage.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.purple.Wish;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+
+import static WorldlySage.MainModfile.makeID;
+
+public class IceWave extends AbstractAbilityCard {
+    public final static String ID = makeID(IceWave.class.getSimpleName());
+
+    public IceWave() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 8;
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
+        Wiz.applyToSelf(new DrawCardNextTurnPower(p, magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Wish.ID;
+    }
+}
