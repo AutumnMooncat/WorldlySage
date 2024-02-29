b566c6e574de5f3ab9cafe36b600ad9f8d28782e
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-02-29 11:45:06 -0400

    Engrave effect now lasts the rest of combat

--- src/main/java/WorldlySage/cards/Engrave.java
+++ src/main/java/WorldlySage/cards/Engrave.java
@@ -1,8 +1,6 @@
 package WorldlySage.cards;
 
-import WorldlySage.actions.ApplyGlyphAction;
 import WorldlySage.actions.BetterSelectCardsInHandAction;
-import WorldlySage.cardmods.CostSwapGlyph;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.Defend_Green;
@@ -24,8 +22,14 @@ public class Engrave extends AbstractEasyCard {
             if (l.size() == 2) {
                 AbstractCard card1 = l.get(0);
                 AbstractCard card2 = l.get(1);
-                addToTop(new ApplyGlyphAction(card1, new CostSwapGlyph(1, card1.cost, card1.costForTurn, card2.cost, card2.costForTurn)));
-                addToTop(new ApplyGlyphAction(card2, new CostSwapGlyph(1, card2.cost, card2.costForTurn, card1.cost, card1.costForTurn)));
+                int cost = card1.cost;
+                int costForTurn = card1.costForTurn;
+                card1.cost = card2.cost;
+                card1.costForTurn = card2.costForTurn;
+                card2.cost = cost;
+                card2.costForTurn = costForTurn;
+                //addToTop(new ApplyGlyphAction(card1, new CostSwapGlyph(1, card1.cost, card1.costForTurn, card2.cost, card2.costForTurn)));
+                //addToTop(new ApplyGlyphAction(card2, new CostSwapGlyph(1, card2.cost, card2.costForTurn, card1.cost, card1.costForTurn)));
             }
         }));
     }
