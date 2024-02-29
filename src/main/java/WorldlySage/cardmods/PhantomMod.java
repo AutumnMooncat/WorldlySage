a99374be0b22b242df6296081c571b11c0eaf661
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-02-29 11:43:56 -0400

    Add Phantom modifier

---
+++ src/main/java/WorldlySage/cardmods/PhantomMod.java
@@ -0,0 +1,35 @@
+package WorldlySage.cardmods;
+
+import basemod.abstracts.AbstractCardModifier;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+
+import static WorldlySage.MainModfile.makeID;
+
+public class PhantomMod extends AbstractCardModifier {
+    public static String ID = makeID(PhantomMod.class.getSimpleName());
+    public static String[] TEXT = CardCrawlGame.languagePack.getCardStrings(ID).EXTENDED_DESCRIPTION;
+
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return TEXT[0] + rawDescription;
+    }
+
+    public boolean shouldApply(AbstractCard card) {
+        return !card.isEthereal;
+    }
+
+    public void onInitialApplication(AbstractCard card) {
+        card.isEthereal = true;
+        if (card.type != AbstractCard.CardType.POWER) {
+            card.exhaust = true;
+        }
+    }
+
+    public AbstractCardModifier makeCopy() {
+        return new PhantomMod();
+    }
+
+    public String identifier(AbstractCard card) {
+        return ID;
+    }
+}
