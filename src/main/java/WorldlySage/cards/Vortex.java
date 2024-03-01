a5aab89218cf300dba22d91d2f8945c7087f8567
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-02-29 21:10:14 -0400

    Rework Vortex

--- src/main/java/WorldlySage/cards/Vortex.java
+++ src/main/java/WorldlySage/cards/Vortex.java
@@ -1,10 +1,13 @@
 package WorldlySage.cards;
 
+import WorldlySage.actions.DoAction;
 import WorldlySage.cardmods.EnergyGlyph;
+import WorldlySage.cardmods.PhantomMod;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
 import WorldlySage.util.Wiz;
 import basemod.helpers.CardModifierManager;
-import com.megacrit.cardcrawl.actions.common.BetterDrawPileToHandAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.Defend_Green;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -16,12 +19,18 @@ public class Vortex extends AbstractEasyCard {
 
     public Vortex() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
-        baseMagicNumber = magicNumber = 1;
+        //baseMagicNumber = magicNumber = 1;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new BetterDrawPileToHandAction(magicNumber));
+        addToBot(new DoAction(() -> {
+            if (!p.drawPile.isEmpty()) {
+                AbstractCard card = p.drawPile.getTopCard().makeStatEquivalentCopy();
+                CardModifierManager.addModifier(card, new PhantomMod());
+                addToTop(new MakeTempCardInHandAction(card));
+            }
+        }));
     }
 
     @Override
