00a9f14a8ecb9adbd850c9fe9c935bc7809ffefd
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-01 12:14:38 -0400

    Buff Vortex

--- src/main/java/WorldlySage/cards/Vortex.java
+++ src/main/java/WorldlySage/cards/Vortex.java
@@ -19,16 +19,18 @@ public class Vortex extends AbstractEasyCard {
 
     public Vortex() {
         super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
-        //baseMagicNumber = magicNumber = 1;
+        baseMagicNumber = magicNumber = 2;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DoAction(() -> {
             if (!p.drawPile.isEmpty()) {
-                AbstractCard card = p.drawPile.getTopCard().makeStatEquivalentCopy();
-                CardModifierManager.addModifier(card, new PhantomMod());
-                addToTop(new MakeTempCardInHandAction(card));
+                for (int i = magicNumber-1 ; i >= 0 ; i--) {
+                    AbstractCard card = p.drawPile.getNCardFromTop(i).makeStatEquivalentCopy();
+                    CardModifierManager.addModifier(card, new PhantomMod());
+                    addToTop(new MakeTempCardInHandAction(card));
+                }
             }
         }));
     }
