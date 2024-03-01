0a3f4dc462df99b2c6d3ba778855261abf7c5d0b
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-01 12:02:01 -0400

    Rework WoodWall

--- src/main/java/WorldlySage/cards/WoodWall.java
+++ src/main/java/WorldlySage/cards/WoodWall.java
@@ -1,33 +1,43 @@
 package WorldlySage.cards;
 
+import WorldlySage.actions.ApplyGlyphAction;
+import WorldlySage.actions.DoAction;
+import WorldlySage.cardmods.AbstractGlyph;
+import WorldlySage.cardmods.ShieldGlyph;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
+import WorldlySage.cards.interfaces.KeepsGlyphsCard;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.cards.green.Defend_Green;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static WorldlySage.MainModfile.makeID;
 
-public class WoodWall extends AbstractEasyCard {
+public class WoodWall extends AbstractEasyCard implements KeepsGlyphsCard {
     public final static String ID = makeID(WoodWall.class.getSimpleName());
 
     public WoodWall() {
         super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
-        baseBlock = block = 4;
+        CardModifierManager.addModifier(this, new ShieldGlyph(2));
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        blck();
-        blck();
+        addToBot(new DoAction(() -> addToBot(new ApplyGlyphAction(this, new ShieldGlyph(1)))));
     }
 
     @Override
     public void upp() {
-        upgradeBlock(2);
+        CardModifierManager.addModifier(this, new ShieldGlyph(1));
     }
 
     @Override
     public String cardArtCopy() {
         return Defend_Green.ID;
+    }
+
+    @Override
+    public boolean shouldKeep(AbstractGlyph glyph) {
+        return glyph instanceof ShieldGlyph;
     }
 }
