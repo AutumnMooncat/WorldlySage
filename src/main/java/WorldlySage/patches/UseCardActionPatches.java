f17c603e960952410cf7b986ff48bcd860ac1bec
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-02 09:08:12 -0400

    Create PlantNextCardPower

--- src/main/java/WorldlySage/patches/UseCardActionPatches.java
+++ src/main/java/WorldlySage/patches/UseCardActionPatches.java
@@ -1,6 +1,7 @@
 package WorldlySage.patches;
 
 import WorldlySage.cards.Wormhole;
+import WorldlySage.ui.PlantedCardManager;
 import basemod.ReflectionHacks;
 import com.badlogic.gdx.Gdx;
 import com.evacipated.cardcrawl.modthespire.lib.*;
@@ -12,6 +13,11 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import javassist.CtBehavior;
 
 public class UseCardActionPatches {
+    @SpirePatch2(clz = UseCardAction.class, method = SpirePatch.CLASS)
+    public static class PlantField {
+        public static SpireField<Boolean> plantNextCard = new SpireField<>(() -> false);
+    }
+
     @SpirePatch2(clz = UseCardAction.class, method = "update")
     public static class FixUCAForWormhole {
         @SpireInsertPatch(locator = Locator.class)
@@ -37,11 +43,37 @@ public class UseCardActionPatches {
             return SpireReturn.Continue();
         }
 
+        @SpireInsertPatch(locator = Locator2.class)
+        public static SpireReturn<?> plz2(UseCardAction __instance, AbstractCard ___targetCard, float ___duration) {
+            if (PlantField.plantNextCard.get(__instance)) {
+                PlantedCardManager.addCard(___targetCard);
+
+                ReflectionHacks.setPrivate(__instance, AbstractGameAction.class, "duration", ___duration - Gdx.graphics.getDeltaTime());
+
+                AbstractDungeon.player.cardInUse = null;
+                ___targetCard.exhaustOnUseOnce = false;
+                ___targetCard.dontTriggerOnUseCard = false;
+                AbstractDungeon.actionManager.addToBottom(new HandCheckAction());
+
+                return SpireReturn.Return();
+            }
+
+            return SpireReturn.Continue();
+        }
+
 
         private static class Locator extends SpireInsertLocator {
             @Override
             public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
                 Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractCard.class, "purgeOnUse");
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+            }
+        }
+
+        private static class Locator2 extends SpireInsertLocator {
+            @Override
+            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(UseCardAction.class, "reboundCard");
                 return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
             }
         }
---
+++ src/main/java/WorldlySage/powers/PlantNextCardPower.java
@@ -0,0 +1,56 @@
+package WorldlySage.powers;
+
+import WorldlySage.MainModfile;
+import WorldlySage.patches.UseCardActionPatches;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class PlantNextCardPower extends AbstractPower {
+    public static final String POWER_ID = MainModfile.makeID(PlantNextCardPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private boolean justEvoked = true;
+
+    public PlantNextCardPower(AbstractCreature owner, int amount) {
+        this.ID = POWER_ID;
+        this.name = NAME;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.loadRegion("rebound");
+        updateDescription();
+    }
+
+    public void updateDescription() {
+        if (this.amount > 1) {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        } else {
+            this.description = DESCRIPTIONS[0];
+        }
+    }
+
+    public void onAfterUseCard(AbstractCard card, UseCardAction action) {
+        if (justEvoked) {
+            justEvoked = false;
+        } else {
+            if (card.type != AbstractCard.CardType.POWER) {
+                flash();
+                UseCardActionPatches.PlantField.plantNextCard.set(action, true);
+            }
+            addToBot(new ReducePowerAction(owner, owner, this, 1));
+        }
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+        }
+    }
+}
