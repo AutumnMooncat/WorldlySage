16d4d29eb53f9d966dd04ecc196ac546eea38d88
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-05 12:43:59 -0400

    Rework Whirlpool

--- src/main/java/WorldlySage/powers/WhirlpoolPower.java
+++ src/main/java/WorldlySage/powers/WhirlpoolPower.java
@@ -1,12 +1,13 @@
 package WorldlySage.powers;
 
 import WorldlySage.MainModfile;
+import WorldlySage.actions.DoAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
@@ -15,6 +16,7 @@ public class WhirlpoolPower extends AbstractPower {
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private boolean active = true;
 
     public WhirlpoolPower(AbstractCreature owner, int amount) {
         this.ID = POWER_ID;
@@ -27,12 +29,30 @@ public class WhirlpoolPower extends AbstractPower {
     }
 
     @Override
+    public void atStartOfTurn() {
+        addToBot(new DoAction(() -> active = false));
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+        addToBot(new DoAction(() -> active = true));
+    }
+
+    @Override
+    public void onCardDraw(AbstractCard card) {
+        if (active) {
+            flashWithoutSound();
+            addToBot(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(amount, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
+        }
+    }
+
+    /*@Override
     public void atEndOfTurnPreEndTurnCards(boolean isPlayer) {
         if (!AbstractDungeon.player.hand.group.isEmpty()) {
             flash();
             addToBot(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(amount * AbstractDungeon.player.hand.size(), true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
         }
-    }
+    }*/
 
     @Override
     public void updateDescription() {
