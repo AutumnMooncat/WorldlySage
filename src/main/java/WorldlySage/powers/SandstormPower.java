b961f3c694e22f6278d7acd830c93475ffcb15ca
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-05 13:17:07 -0400

    Rework Sandstorm

--- src/main/java/WorldlySage/powers/SandstormPower.java
+++ src/main/java/WorldlySage/powers/SandstormPower.java
@@ -1,16 +1,19 @@
 package WorldlySage.powers;
 
 import WorldlySage.MainModfile;
+import WorldlySage.actions.DoAction;
+import WorldlySage.patches.SandstormPatch;
 import WorldlySage.vfx.ColoredSmokeBombEffect;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
+import com.megacrit.cardcrawl.powers.GainStrengthPower;
 
 public class SandstormPower extends AbstractPower {
     public static final String POWER_ID = MainModfile.makeID(SandstormPower.class.getSimpleName());
@@ -33,11 +36,22 @@ public class SandstormPower extends AbstractPower {
         this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
-    public void onExhaust(AbstractCard card) {
+    public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+        if (power.type == PowerType.DEBUFF && !power.ID.equals(GainStrengthPower.POWER_ID) && source == this.owner && target != this.owner && !target.hasPower(ArtifactPower.POWER_ID)) {
+            //Don't infinite loop with Wave of the Hand
+            flash();
+            addToBot(new DoAction(() -> SandstormPatch.looping = true));
+            addToBot(new GainBlockAction(this.owner, this.amount, Settings.FAST_MODE));
+            addToBot(new VFXAction(new ColoredSmokeBombEffect(owner.hb.cX, owner.hb.cY, Color.BROWN.cpy())));
+            addToBot(new DoAction(() -> SandstormPatch.looping = false));
+        }
+    }
+
+    /*public void onExhaust(AbstractCard card) {
         flashWithoutSound();
         addToBot(new GainBlockAction(this.owner, this.amount, Settings.FAST_MODE));
         addToBot(new VFXAction(new ColoredSmokeBombEffect(owner.hb.cX, owner.hb.cY, Color.BROWN.cpy())));
-    }
+    }*/
 
     /*@Override
     public void atEndOfTurn(boolean isPlayer) {
