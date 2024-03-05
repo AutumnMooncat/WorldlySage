cbc058540b7d62267dc45e2fb9523469b3c2a68a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-04 22:36:23 -0400

    Rework Manastorm

--- src/main/java/WorldlySage/powers/ManaStormPower.java
+++ src/main/java/WorldlySage/powers/ManaStormPower.java
@@ -6,6 +6,8 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+import com.megacrit.cardcrawl.powers.EnergizedBluePower;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public class ManaStormPower extends AbstractPower {
@@ -26,14 +28,19 @@ public class ManaStormPower extends AbstractPower {
 
     @Override
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        if (amount == 1) {
+            this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount + DESCRIPTIONS[2];
+        } else {
+            this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount + DESCRIPTIONS[3];
+        }
     }
 
     @Override
     public void atEndOfTurn(boolean isPlayer) {
-        if (EnergyPanel.getCurrentEnergy() > 0) {
+        if (isPlayer && EnergyPanel.getCurrentEnergy() > 0) {
             flash();
-            Wiz.applyToSelf(new MysticForcePower(Wiz.adp(), amount));
+            Wiz.applyToSelf(new EnergizedBluePower(Wiz.adp(), amount));
+            Wiz.applyToSelf(new DrawCardNextTurnPower(Wiz.adp(), amount));
         }
     }
 }
