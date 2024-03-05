7331b073be11f7a7c95191efad22b6a5ecc6fdc2
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-05 10:49:44 -0400

    Mudball now SuckerPunch clone

--- src/main/java/WorldlySage/cards/Mudball.java
+++ src/main/java/WorldlySage/cards/Mudball.java
@@ -18,8 +18,8 @@ public class Mudball extends AbstractEasyCard {
 
     public Mudball() {
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 6;
-        baseMagicNumber = magicNumber = 2;
+        baseDamage = damage = 7;
+        baseMagicNumber = magicNumber = 1;
     }
 
     @Override
