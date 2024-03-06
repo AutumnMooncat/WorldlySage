1dddcd18742bce812953b73fda17cb8e8d04a302
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-06 18:42:35 -0400

    Rework GrassyPlain

--- src/main/java/WorldlySage/cards/GrassyPlain.java
+++ src/main/java/WorldlySage/cards/GrassyPlain.java
@@ -1,6 +1,7 @@
 package WorldlySage.cards;
 
 import WorldlySage.cards.abstracts.AbstractEasyCard;
+import WorldlySage.powers.GrowNextCardPower;
 import WorldlySage.powers.PlantNextCardPower;
 import WorldlySage.util.Wiz;
 import com.megacrit.cardcrawl.cards.purple.InnerPeace;
@@ -15,18 +16,23 @@ public class GrassyPlain extends AbstractEasyCard {
     public GrassyPlain() {
         super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
         baseBlock = block = 3;
+        baseMagicNumber = magicNumber = 0;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
         Wiz.applyToSelf(new PlantNextCardPower(p, 1));
+        if (magicNumber > 0) {
+            Wiz.applyToSelf(new GrowNextCardPower(p, magicNumber));
+        }
     }
 
     @Override
     public void upp() {
-        //upgradeBlock(2);
-        upgradeBaseCost(0);
+        upgradeBlock(1);
+        upgradeMagicNumber(2);
+        //upgradeBaseCost(0);
     }
 
     @Override
