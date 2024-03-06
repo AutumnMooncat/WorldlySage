bf453ee05026c9d570faa78c125b70e694b28e33
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-06 15:02:20 -0400

    Rework Meteor

--- src/main/java/WorldlySage/cards/Meteor.java
+++ src/main/java/WorldlySage/cards/Meteor.java
@@ -1,12 +1,11 @@
 package WorldlySage.cards;
 
-import WorldlySage.cardmods.PiercingGlyph;
+import WorldlySage.actions.DamageFollowupAction;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
-import WorldlySage.util.Wiz;
-import basemod.helpers.CardModifierManager;
+import WorldlySage.vfx.BigExplosionVFX;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.purple.Wish;
@@ -21,7 +20,7 @@ public class Meteor extends AbstractEasyCard {
 
     public Meteor() {
         super(ID, 2, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = damage = 18;
+        baseDamage = damage = 20;
     }
 
     @Override
@@ -30,30 +29,17 @@ public class Meteor extends AbstractEasyCard {
             this.addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
         }
         this.addToBot(new WaitAction(0.8F));
-        this.addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
-    }
-
-    @Override
-    public void applyPowers() {
-        super.applyPowers();
-        if (Wiz.hasGlyph(this)) {
-            damage *= 2;
-            isDamageModified = damage != baseDamage;
-        }
-    }
-
-    @Override
-    public void calculateCardDamage(AbstractMonster mo) {
-        super.calculateCardDamage(mo);
-        if (Wiz.hasGlyph(this)) {
-            damage *= 2;
-            isDamageModified = damage != baseDamage;
-        }
+        this.addToBot(new DamageFollowupAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, false, t -> {
+            if (t.isDying || t.currentHealth <= 0) {
+                addToTop(new DamageAllEnemiesAction(p, DamageInfo.createDamageMatrix(damage, true), damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+                addToTop(new BigExplosionVFX(t));
+            }
+        }));
     }
 
     @Override
     public void upp() {
-        CardModifierManager.addModifier(this, new PiercingGlyph(1));
+        upgradeDamage(8);
     }
 
     @Override
