139b77030dfd4a01f5de2fa158a0f4295829036f
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-07 09:28:28 -0400

    Rework CrystalShards

--- src/main/java/WorldlySage/cards/CrystalShards.java
+++ src/main/java/WorldlySage/cards/CrystalShards.java
@@ -2,7 +2,6 @@ package WorldlySage.cards;
 
 import WorldlySage.cards.abstracts.AbstractEasyCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.purple.Wish;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -13,22 +12,21 @@ public class CrystalShards extends AbstractEasyCard {
     public final static String ID = makeID(CrystalShards.class.getSimpleName());
 
     public CrystalShards() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 2;
-        baseMagicNumber = magicNumber = 1;
+        super(ID, 0, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 3;
+        baseMagicNumber = magicNumber = 2;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
-        addToBot(new DrawCardAction(magicNumber));
+        for (int i = 0 ; i < magicNumber ; i++) {
+            dmg(m, i%3 == 0 ? AbstractGameAction.AttackEffect.SLASH_VERTICAL : i%3 == 1 ? AbstractGameAction.AttackEffect.SLASH_DIAGONAL : AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+        }
     }
 
     @Override
     public void upp() {
-        upgradeDamage(1);
+        upgradeMagicNumber(1);
     }
 
     @Override
