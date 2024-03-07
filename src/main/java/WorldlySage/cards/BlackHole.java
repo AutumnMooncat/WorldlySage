701584f3e8f735be8b4be799a62c00b2a3e6d2e2
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-07 19:37:32 -0400

    Rework BlackHole, BoulderCrash

--- src/main/java/WorldlySage/cards/BlackHole.java
+++ src/main/java/WorldlySage/cards/BlackHole.java
@@ -3,6 +3,7 @@ package WorldlySage.cards;
 import WorldlySage.actions.DamageFollowupAction;
 import WorldlySage.actions.ThrowObjectAction;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
+import WorldlySage.patches.EnterCardGroupPatches;
 import WorldlySage.powers.CrushPower;
 import WorldlySage.util.TextureScaler;
 import WorldlySage.util.Wiz;
@@ -13,6 +14,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.purple.Wish;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -23,14 +25,15 @@ import com.megacrit.cardcrawl.vfx.WallopEffect;
 
 import static WorldlySage.MainModfile.makeID;
 
-public class BlackHole extends AbstractEasyCard {
+public class BlackHole extends AbstractEasyCard implements EnterCardGroupPatches.OnEnterCardGroupCard {
     public final static String ID = makeID(BlackHole.class.getSimpleName());
     private static final Texture STAR_TEX = TextureScaler.rescale(ImageMaster.TINY_STAR, 1, 1);
+    private CardGroup lastGroup;
 
     public BlackHole() {
         super(ID, 3, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = damage = 20;
-        baseMagicNumber = magicNumber = 5;
+        baseDamage = damage = 25;
+        baseMagicNumber = magicNumber = 3;
         //exhaust = true;
     }
 
@@ -55,18 +58,28 @@ public class BlackHole extends AbstractEasyCard {
                 public void dispose() {}
             }));
             Wiz.atb(new DamageFollowupAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY, true, mon -> addToTop(new VFXAction(new WallopEffect(mon.lastDamageTaken, mon.hb.cX, mon.hb.cY)))));
-            Wiz.applyToEnemy(m, new CrushPower(m, magicNumber));
         }
     }
 
     @Override
     public void upp() {
-        upgradeDamage(8);
+        upgradeDamage(10);
         upgradeMagicNumber(2);
     }
 
     @Override
     public String cardArtCopy() {
         return Wish.ID;
+    }
+
+    @Override
+    public void onEnter(CardGroup g) {
+        if (g != lastGroup) {
+            lastGroup = g;
+            if (g == Wiz.adp().hand) {
+                superFlash();
+                Wiz.forAllMonstersLiving(mon -> Wiz.applyToEnemy(mon, new CrushPower(mon, magicNumber)));
+            }
+        }
     }
 }
--- src/main/java/WorldlySage/cards/BoulderCrash.java
+++ src/main/java/WorldlySage/cards/BoulderCrash.java
@@ -1,12 +1,12 @@
 package WorldlySage.cards;
 
 import WorldlySage.cards.abstracts.AbstractEasyCard;
+import WorldlySage.powers.CrushPower;
 import WorldlySage.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.purple.Wish;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static WorldlySage.MainModfile.makeID;
 
@@ -15,19 +15,19 @@ public class BoulderCrash extends AbstractEasyCard {
 
     public BoulderCrash() {
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
-        baseDamage = damage = 12;
-        baseMagicNumber = magicNumber = 2;
+        baseDamage = damage = 13;
+        baseMagicNumber = magicNumber = 4;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        Wiz.applyToEnemy(m, new VulnerablePower(m, magicNumber, false));
+        Wiz.applyToEnemy(m, new CrushPower(m, magicNumber));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(3);
+        upgradeDamage(4);
         upgradeMagicNumber(1);
     }
 
