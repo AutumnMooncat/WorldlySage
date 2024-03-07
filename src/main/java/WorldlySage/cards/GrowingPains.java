d327876e942ec670b4ec779f2b14b5c7fd074ff9
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-07 16:30:26 -0400

    Adjust GrowingPains

--- src/main/java/WorldlySage/cards/GrowingPains.java
+++ src/main/java/WorldlySage/cards/GrowingPains.java
@@ -1,7 +1,9 @@
 package WorldlySage.cards;
 
+import WorldlySage.actions.ApplyGrowthAction;
 import WorldlySage.actions.PlantCardsAction;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.green.BladeDance;
 import com.megacrit.cardcrawl.cards.tempCards.Shiv;
@@ -23,14 +25,19 @@ public class GrowingPains extends AbstractEasyCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         CardGroup group = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
         for (int i = 0 ; i < magicNumber ; i++) {
-            group.addToTop(new Shiv());
+            AbstractCard card =  new Shiv();
+            group.addToTop(card);
+            if (upgraded) {
+                ApplyGrowthAction.applyGrowth(card, magicNumber);
+            }
         }
         addToBot(new PlantCardsAction(group, group.size()));
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(1);
+        //upgradeMagicNumber(1);
+        uDesc();
     }
 
     @Override
