3b13e6b077b91987bc652bc244a3c4eb35009a17
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-09 19:14:49 -0400

    Irrigation now grows Planted cards at the start of your turn.

--- src/main/java/WorldlySage/powers/HydrationPower.java
+++ src/main/java/WorldlySage/powers/HydrationPower.java
@@ -1,14 +1,15 @@
 package WorldlySage.powers;
 
 import WorldlySage.MainModfile;
-import WorldlySage.powers.interfaces.OnGrowthPower;
+import WorldlySage.actions.ApplyGrowthAction;
+import WorldlySage.ui.PlantedCardManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class HydrationPower extends AbstractPower implements OnGrowthPower {
+public class HydrationPower extends AbstractPower {
     public static final String POWER_ID = MainModfile.makeID(HydrationPower.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
@@ -26,11 +27,23 @@ public class HydrationPower extends AbstractPower implements OnGrowthPower {
 
     @Override
     public void updateDescription() {
-        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        if (amount == 1) {
+            this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        } else {
+            this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[2];
+        }
     }
 
     @Override
+    public void atStartOfTurn() {
+        flash();
+        for (AbstractCard c : PlantedCardManager.cards.group) {
+            ApplyGrowthAction.applyGrowth(c, amount);
+        }
+    }
+
+    /*@Override
     public int onGrow(AbstractCard card, int amount) {
         return amount + this.amount;
-    }
+    }*/
 }
