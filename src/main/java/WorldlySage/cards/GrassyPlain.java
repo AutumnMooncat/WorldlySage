d6a43f1df0f374bcc52dda3808b381000363688f
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-09 19:13:29 -0400

    Rework GrassyPlain, Till

--- src/main/java/WorldlySage/cards/GrassyPlain.java
+++ src/main/java/WorldlySage/cards/GrassyPlain.java
@@ -1,9 +1,8 @@
 package WorldlySage.cards;
 
+import WorldlySage.actions.PlantCardsAction;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
-import WorldlySage.powers.GrowNextCardPower;
-import WorldlySage.powers.PlantNextCardPower;
-import WorldlySage.util.Wiz;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.purple.InnerPeace;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -14,24 +13,26 @@ public class GrassyPlain extends AbstractEasyCard {
     public final static String ID = makeID(GrassyPlain.class.getSimpleName());
 
     public GrassyPlain() {
-        super(ID, 1, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
-        baseBlock = block = 3;
-        baseMagicNumber = magicNumber = 0;
+        super(ID, 2, CardType.SKILL, CardRarity.BASIC, CardTarget.SELF);
+        baseBlock = block = 6;
+        baseMagicNumber = magicNumber = 1;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         blck();
-        Wiz.applyToSelf(new PlantNextCardPower(p, 1));
-        if (magicNumber > 0) {
-            Wiz.applyToSelf(new GrowNextCardPower(p, magicNumber));
+        CardGroup group = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+        for (int i = 0 ; i < magicNumber ; i++) {
+            group.addToTop(new Sapling());
         }
+        addToBot(new PlantCardsAction(group, group.size()));
+
     }
 
     @Override
     public void upp() {
-        upgradeBlock(1);
-        upgradeMagicNumber(2);
+        //upgradeBlock(2);
+        upgradeMagicNumber(1);
         //upgradeBaseCost(0);
     }
 
--- src/main/java/WorldlySage/cards/Till.java
+++ src/main/java/WorldlySage/cards/Till.java
@@ -1,9 +1,9 @@
 package WorldlySage.cards;
 
-import WorldlySage.actions.PlantCardsAction;
+import WorldlySage.actions.ApplyGrowthToAllCardsAction;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
+import WorldlySage.ui.PlantedCardManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.purple.Wish;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -14,27 +14,28 @@ public class Till extends AbstractEasyCard {
     public final static String ID = makeID(Till.class.getSimpleName());
 
     public Till() {
-        super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        super(ID, 0, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
         baseDamage = damage = 3;
-        baseMagicNumber = magicNumber = 1;
-        cardsToPreview = new Sapling();
+        baseMagicNumber = magicNumber = 2;
+        //cardsToPreview = new Sapling();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SLASH_HEAVY);
-        CardGroup group = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+        /*CardGroup group = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
         for (int i = 0 ; i < magicNumber ; i++) {
             group.addToTop(new Sapling());
         }
-        addToBot(new PlantCardsAction(group, group.size()));
+        addToBot(new PlantCardsAction(group, group.size()));*/
+        addToBot(new ApplyGrowthToAllCardsAction(magicNumber, PlantedCardManager.cards::contains));
     }
 
     @Override
     public void upp() {
-        //upgradeDamage(2);
-        //upgradeMagicNumber(1);
-        upgradeBaseCost(0);
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
+        //upgradeBaseCost(0);
     }
 
     @Override
