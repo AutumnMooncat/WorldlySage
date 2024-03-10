9dd72672e51b754f799fcb9cae0f63050270b3a6
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-10 15:38:14 -0300

    RiverOfAsh no longer Exhausts a card.

--- src/main/java/WorldlySage/cards/RiverOfAsh.java
+++ src/main/java/WorldlySage/cards/RiverOfAsh.java
@@ -3,7 +3,6 @@ package WorldlySage.cards;
 import WorldlySage.cards.abstracts.AbstractEasyCard;
 import WorldlySage.powers.RiverOfAshPower;
 import WorldlySage.util.Wiz;
-import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.green.Defend_Green;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -22,7 +21,7 @@ public class RiverOfAsh extends AbstractEasyCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         Wiz.applyToSelf(new RiverOfAshPower(p, magicNumber));
-        addToBot(new ExhaustAction(1, false, false));
+        //addToBot(new ExhaustAction(1, false, false));
     }
 
     @Override
